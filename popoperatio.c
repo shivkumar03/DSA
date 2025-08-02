a4418dc7dea0c17d1d5b3aaf9deaf7f64de2d0da
shivkumar03 <shivprajapati2060@gmail.com>
2025-08-02 18:57:49 +0530

    pop operation

---
+++ popoperatio.c
@@ -0,0 +1,90 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct stack 
+{
+    int size;
+    int top;
+    int *arr;
+};
+
+int Empty(struct stack *ptr)
+{
+    if(ptr->top==-1)
+    {
+        return -1;
+    }
+    else{
+        return 0;
+    }
+}
+ int Full(struct stack *ptr)
+ {
+    if(ptr->top==ptr-> size-1)
+    {
+        return 1;
+    }
+    else{
+        return 0;
+    }
+ }
+ void push(struct stack*ptr, int val)
+ {
+    if(Full(ptr))
+    {
+        printf("stack overflow cannot push %d to the stack \n", val);
+    }
+    else{
+        ptr->top++;
+        ptr->arr[ptr->top]=val;
+    } 
+ }
+
+ int pop(struct stack*ptr)
+ {
+    if(Empty(ptr))
+    {
+    printf("stsck underflow! cannot pop from the stack\n");
+    return -1;
+    }
+
+  else
+  {
+    int val = ptr->arr[ptr->top];
+    ptr->top--;
+    return val;
+  }
+ }
+ int main()
+ {
+    struct stack *sp=(struct stack*)malloc(sizeof (struct stack)); 
+    sp-> size = 10;
+    sp-> top = -1;
+    sp-> arr=(int*)malloc(sp->size*sizeof (int));
+
+    printf("stack has been created succesfully\n");
+    printf("before pushing , Full %d\n", Full(sp));
+    printf("before pushing , Empty %d\n", Empty(sp));
+    
+    push(sp, 56);
+    push(sp, 5);    
+    push(sp, 45);
+    push(sp, 6);
+    push(sp, 77);
+    push(sp, 76);
+    push(sp, 52);
+    push(sp, 97);
+    push(sp, 23);
+    push(sp, 13); // push 10 value
+    //push(sp, 44); // stack overflow
+
+    printf("after pushing Full %d\n", Full(sp));
+    printf("after pushiung Empty %d\n", Empty(sp));
+
+    printf("popped %d from the stack\n", pop(sp));
+    printf("popped %d from the stack\n", pop(sp));
+    printf("popped %d from the stack\n", pop(sp));
+
+ 
+    return 0;
+ }
